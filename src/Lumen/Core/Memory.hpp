c75a5a67ca3261d4ad7ebd1ef79ebbf83dbfe97e
Vinicius Augusto <vinicius.borgue1@gmail.com>
2024-09-17 11:48:47 -0300

    feat(Core): added final memory implementations

--- src/Lumen/Core/Memory.hpp
+++ src/Lumen/Core/Memory.hpp
@@ -6,15 +6,15 @@ namespace Lumen
 {
 
 template <typename T> using Scope = std::unique_ptr<T>;
-template <typename T> Scope<T> CreateScope()
+template <typename T, typename... Args> constexpr Scope<T> CreateScope(Args &&...args)
 {
-    return std::make_unique<T>();
+    return std::make_unique<T>(std::forward<Args>(args)...);
 }
 
 template <typename T> using Ref = std::shared_ptr<T>;
-template <typename T> Scope<T> CreateRef()
+template <typename T, typename... Args> constexpr Ref<T> CreateRef(Args &&...args)
 {
-    return std::make_shared<T>();
+    return std::make_shared<T>(std::forward<Args>(args)...);
 }
 
 } // namespace Lumen
