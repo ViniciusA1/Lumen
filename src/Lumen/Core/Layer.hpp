e01796cc8c4515f03e8be34c98e31ffd7234118e
Vinicius Augusto <vinicius.borgue1@gmail.com>
2024-10-18 13:42:36 -0300

    feat(Core): added EventListeners to Layer

--- src/Lumen/Core/Layer.hpp
+++ src/Lumen/Core/Layer.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "Lumen/Event/KeyboardListener.hpp"
+#include "Lumen/Event/MouseListener.hpp"
 #include <string>
+#include <vector>
 
 namespace Lumen
 {
@@ -15,6 +18,11 @@ public:
     [[nodiscard]] bool IsVisible() const { return m_IsVisible; }
     void SetVisible(bool visible) { m_IsVisible = visible; }
 
+    std::vector<KeyboardListener> &GetKeyboardListeners() { return m_KeyboardListener; }
+    std::vector<MouseListener> &GetMouseListeners() { return m_MouseListener; }
+    void AddKeyboardListener(const KeyboardListener &listener);
+    void AddMouseListener(const MouseListener &listener);
+
     virtual void OnAttach() {}
     virtual void OnDetach() {}
     virtual void OnEvent() {}
@@ -24,6 +32,9 @@ public:
 private:
     std::string m_Name;
     bool m_IsVisible;
+
+    std::vector<KeyboardListener> m_KeyboardListener;
+    std::vector<MouseListener> m_MouseListener;
 };
 
 } // namespace Lumen
