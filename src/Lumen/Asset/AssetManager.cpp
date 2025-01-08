512bbdf09030f4caddbc501b3526bea2be4c62d6
Vinicius Augusto <vinicius.borgue1@gmail.com>
2025-01-08 17:19:45 -0300

    feat(Asset): added asset name get function and working directory path

--- src/Lumen/Asset/AssetManager.cpp
+++ src/Lumen/Asset/AssetManager.cpp
@@ -3,7 +3,18 @@
 namespace Lumen
 {
 
-std::map<std::type_index, std::map<UUID, Ref<Asset>>> AssetManager::m_AssetCache;
-std::map<std::type_index, Ref<Asset>> AssetManager::m_DefaultAsset;
+std::map<std::type_index, std::map<UUID, Ref<Asset>>> AssetManager::s_AssetCache;
+std::map<std::type_index, Ref<Asset>> AssetManager::s_DefaultAsset;
+Path AssetManager::s_WorkingDirectory;
+
+Path AssetManager::GetWorkingDirectory()
+{
+    return s_WorkingDirectory;
+}
+
+void AssetManager::SetWorkingDirectory(const Path &path)
+{
+    s_WorkingDirectory = path;
+}
 
 } // namespace Lumen
--- src/Lumen/Asset/AssetManager.hpp
+++ src/Lumen/Asset/AssetManager.hpp
@@ -12,58 +12,96 @@ namespace Lumen
 class AssetManager
 {
 public:
-    template <typename T> static Ref<T> Load(const AssetMetadata &metadata)
+    template <typename T> static inline Ref<T> Get(UUID uuid);
+    template <typename T> static inline Ref<T> Get(const std::string &name);
+    template <typename T> static inline Ref<T> Load(const AssetMetadata &metadata);
+
+    static Path GetWorkingDirectory();
+    static void SetWorkingDirectory(const Path &path);
+
+private:
+    static std::map<std::type_index, std::map<UUID, Ref<Asset>>> s_AssetCache;
+    static std::map<std::type_index, Ref<Asset>> s_DefaultAsset;
+    static Path s_WorkingDirectory;
+};
+
+template <typename T> Ref<T> AssetManager::Get(UUID uuid)
+{
+    auto typeCacheIt = s_AssetCache.find(typeid(T));
+    if (typeCacheIt == s_AssetCache.end())
     {
-        auto &typeCache = m_AssetCache[typeid(T)];
-        auto it = typeCache.find(metadata.ID);
-        if (it != typeCache.end())
+        auto defaultIt = s_DefaultAsset.find(typeid(T));
+        if (defaultIt != s_DefaultAsset.end())
         {
-            return std::dynamic_pointer_cast<T>(it->second);
+            return std::dynamic_pointer_cast<T>(defaultIt->second);
         }
-
-        Ref<Asset> asset = AssetImporter::ImportAsset(typeid(T), metadata);
-        if (asset == nullptr)
-        {
-            auto defaultIt = m_DefaultAsset.find(typeid(T));
-            if (defaultIt != m_DefaultAsset.end())
-            {
-                return std::dynamic_pointer_cast<T>(defaultIt->second);
-            }
-        }
-
-        typeCache[metadata.ID] = asset;
-        return std::dynamic_pointer_cast<T>(asset);
     }
 
-    template <typename T> static Ref<T> Get(UUID uuid)
+    const auto &typeCache = typeCacheIt->second;
+    auto it = typeCache.find(uuid);
+    if (it == typeCache.end())
     {
-        auto typeCacheIt = m_AssetCache.find(typeid(T));
-        if (typeCacheIt == m_AssetCache.end())
+        auto defaultIt = s_DefaultAsset.find(typeid(T));
+        if (defaultIt != s_DefaultAsset.end())
         {
-            auto defaultIt = m_DefaultAsset.find(typeid(T));
-            if (defaultIt != m_DefaultAsset.end())
-            {
-                return std::dynamic_pointer_cast<T>(defaultIt->second);
-            }
+            return std::dynamic_pointer_cast<T>(defaultIt->second);
         }
+    }
 
-        const auto &typeCache = typeCacheIt->second;
-        auto it = typeCache.find(uuid);
-        if (it == typeCache.end())
+    return std::dynamic_pointer_cast<T>(it->second);
+}
+
+template <typename T> Ref<T> AssetManager::Get(const std::string &name)
+{
+    auto typeCacheIt = s_AssetCache.find(typeid(T));
+    if (typeCacheIt == s_AssetCache.end())
+    {
+        auto defaultIt = s_DefaultAsset.find(typeid(T));
+        if (defaultIt != s_DefaultAsset.end())
         {
-            auto defaultIt = m_DefaultAsset.find(typeid(T));
-            if (defaultIt != m_DefaultAsset.end())
-            {
-                return std::dynamic_pointer_cast<T>(defaultIt->second);
-            }
+            return std::dynamic_pointer_cast<T>(defaultIt->second);
         }
+    }
 
+    const auto &typeCache = typeCacheIt->second;
+
+    for (const auto &[uuid, asset] : typeCache)
+    {
+        if (asset->GetMetadata().Name == name)
+        {
+            return std::dynamic_pointer_cast<T>(asset);
+        }
+    }
+
+    auto defaultIt = s_DefaultAsset.find(typeid(T));
+    if (defaultIt != s_DefaultAsset.end())
+    {
+        return std::dynamic_pointer_cast<T>(defaultIt->second);
+    }
+}
+
+template <typename T> Ref<T> AssetManager::Load(const AssetMetadata &metadata)
+{
+    auto &typeCache = s_AssetCache[typeid(T)];
+    auto it = typeCache.find(metadata.ID);
+    if (it != typeCache.end())
+    {
         return std::dynamic_pointer_cast<T>(it->second);
     }
 
-private:
-    static std::map<std::type_index, std::map<UUID, Ref<Asset>>> m_AssetCache;
-    static std::map<std::type_index, Ref<Asset>> m_DefaultAsset;
-};
+    AssetMetadata data = {metadata.ID, metadata.Name, s_WorkingDirectory / metadata.Path};
+    Ref<Asset> asset = AssetImporter::ImportAsset(typeid(T), data);
+    if (asset == nullptr)
+    {
+        auto defaultIt = s_DefaultAsset.find(typeid(T));
+        if (defaultIt != s_DefaultAsset.end())
+        {
+            return std::dynamic_pointer_cast<T>(defaultIt->second);
+        }
+    }
+
+    typeCache[metadata.ID] = asset;
+    return std::dynamic_pointer_cast<T>(asset);
+}
 
 } // namespace Lumen
