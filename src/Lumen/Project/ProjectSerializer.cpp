d19bede77ed14bd2b3d2a948c12c32b1f16e014d
Vinicius Augusto <vinicius.borgue1@gmail.com>
2025-01-21 17:26:56 -0300

    refactor(Project): changed project serializer to support new serialization system

--- src/Lumen/Project/ProjectSerializer.cpp
+++ src/Lumen/Project/ProjectSerializer.cpp
@@ -1,7 +1,6 @@
 #include "Lumen/Project/ProjectSerializer.hpp"
 
-#include "yaml-cpp/yaml.h"
-#include <fstream>
+#include "Lumen/File/Serializer/YamlSerializer.hpp"
 
 namespace Lumen
 {
@@ -10,21 +9,12 @@ bool ProjectSerializer::Deserialize(Project &project, const Path &path)
 {
     Path configPath = path / "ProjectConfig.lproj";
 
-    try
-    {
-        YAML::Node config = YAML::LoadFile(configPath.String());
-
-        project.Name = config["Name"].as<std::string>();
-        project.LastModified = config["LastModified"].as<std::string>();
-        project.RootDirectory = path;
-        project.WorkingDirectory = config["WorkingDirectory"].as<std::string>();
-        project.AssetDirectory = config["AssetDirectory"].as<std::string>();
-        project.StartScene = config["StartScene"].as<std::string>();
-    }
-    catch (const YAML::Exception &e)
-    {
+    Yaml yaml = Yaml::FromFile(configPath);
+    if (yaml.IsNull())
         return false;
-    }
+
+    yaml >> project;
+    project.RootDirectory = path;
 
     return true;
 }
@@ -33,24 +23,9 @@ bool ProjectSerializer::Serialize(const Project &project, const Path &path)
 {
     Path configPath = path / "ProjectConfig.lproj";
 
-    try
-    {
-        YAML::Node configNode;
-        configNode["Name"] = project.Name;
-        configNode["LastModified"] = project.LastModified;
-        configNode["WorkingDirectory"] = project.WorkingDirectory.String();
-        configNode["AssetDirectory"] = project.AssetDirectory.String();
-        configNode["StartScene"] = project.StartScene.String();
-
-        std::ofstream configFile(configPath.String());
-        configFile << configNode;
-    }
-    catch (const YAML::Exception &e)
-    {
-        return false;
-    }
-
-    return true;
+    Yaml yaml;
+    yaml << project;
+    return yaml.ToFile(configPath);
 }
 
 } // namespace Lumen
