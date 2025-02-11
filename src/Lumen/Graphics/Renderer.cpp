5eef2ef62d0314edeb0ae8c38ede14140b79d6bb
Vinicius Augusto <vinicius.borgue1@gmail.com>
2025-02-11 08:23:00 -0300

    feat(Graphics): added a bunch of geometric draw functions

--- src/Lumen/Graphics/Renderer.cpp
+++ src/Lumen/Graphics/Renderer.cpp
@@ -1,7 +1,9 @@
 #include "Lumen/Graphics/Renderer.hpp"
-
 #include "Lumen/Math/Math.hpp"
+#include "Lumen/Math/Matrix4.hpp"
+
 #include "raylib.h"
+#include "raymath.h"
 #include "rlgl.h"
 
 namespace Lumen
@@ -107,23 +109,135 @@ void Renderer::DrawFPS(const Vector2 position, int size, Color color)
     ::DrawText(fps.c_str(), position.x, position.y, size, color);
 }
 
-void Renderer::DrawQuad(const TransformComponent &transform, Color color)
+void Renderer::DrawCapsule(const TransformComponent &transform, int slices, int rings,
+                           Color color)
 {
+    rlPushMatrix();
+
+    rlTranslatef(transform.Position.x, transform.Position.y, transform.Position.z);
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
+    rlScalef(transform.Scale.x, transform.Scale.y, transform.Scale.z);
+
+    ::DrawCapsule({0.0f, -0.5f, 0.0f}, {0.0f, 0.5f, 0.0f}, 0.5f, slices, rings, color);
+
+    rlPopMatrix();
+}
+
+void Renderer::DrawCapsuleWire(const TransformComponent &transform, int slices, int rings,
+                               Color color)
+{
+    rlPushMatrix();
+
+    rlTranslatef(transform.Position.x, transform.Position.y, transform.Position.z);
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
+    rlScalef(transform.Scale.x, transform.Scale.y, transform.Scale.z);
+
+    ::DrawCapsuleWires({0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, 0.5f, slices, rings,
+                       color);
+
+    rlPopMatrix();
+}
+
+void Renderer::DrawCircle(const TransformComponent &transform, Color color)
+{
+    rlPushMatrix();
+
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
+    rlScalef(transform.Scale.x, transform.Scale.y, transform.Scale.z);
+
+    ::DrawCircle3D(transform.Position, 1.0f, {0.0f, 0.0f, 0.0f}, 0, color);
+
+    rlPopMatrix();
 }
 
 void Renderer::DrawCube(const TransformComponent &transform, Color color)
 {
     rlPushMatrix();
 
-    rlTranslatef(transform.Position.x, transform.Position.y, transform.Position.z);
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
+
+    ::DrawCubeV(transform.Position, transform.Scale, color);
+
+    rlPopMatrix();
+}
+
+void Renderer::DrawCubeWire(const TransformComponent &transform, Color color)
+{
+    rlPushMatrix();
 
     rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
     rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
     rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
 
+    ::DrawCubeWiresV(transform.Position, transform.Scale, color);
+
+    rlPopMatrix();
+}
+
+void Renderer::DrawCylinder(const TransformComponent &transform, int slices, Color color)
+{
+    rlPushMatrix();
+
+    rlTranslatef(transform.Position.x, transform.Position.y, transform.Position.z);
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
     rlScalef(transform.Scale.x, transform.Scale.y, transform.Scale.z);
 
-    ::DrawCube(Vector3::Zero, 1.0f, 1.0f, 1.0f, color);
+    ::DrawCylinder({0.0f, -0.5f, 0.0f}, 1.0f, 1.0f, 1.0f, slices, color);
+
+    rlPopMatrix();
+}
+
+void Renderer::DrawCylinder(const TransformComponent &transform, float radiusTop,
+                            float radiusBottom, int slices, Color color)
+{
+    rlPushMatrix();
+
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
+    rlScalef(transform.Scale.x, transform.Scale.y, transform.Scale.z);
+
+    ::DrawCylinder(transform.Position, radiusTop, radiusBottom, 1.0f, slices, color);
+
+    rlPopMatrix();
+}
+
+void Renderer::DrawCylinderWire(const TransformComponent &transform, int slices,
+                                Color color)
+{
+    rlPushMatrix();
+
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
+    rlScalef(transform.Scale.x, transform.Scale.y, transform.Scale.z);
+
+    ::DrawCylinderWires(transform.Position, 1.0f, 1.0f, 1.0f, slices, color);
+
+    rlPopMatrix();
+}
+
+void Renderer::DrawCylinderWire(const TransformComponent &transform, float radiusTop,
+                                float radiusBottom, int slices, Color color)
+{
+    rlPushMatrix();
+
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
+    rlScalef(transform.Scale.x, transform.Scale.y, transform.Scale.z);
+
+    ::DrawCylinderWires(transform.Position, radiusTop, radiusBottom, 1.0f, slices, color);
 
     rlPopMatrix();
 }
@@ -158,6 +272,110 @@ void Renderer::DrawGrid(Vector3 cameraPosition, float minorSpacing, int majorDiv
         Color lineColor = isMajorLine ? LIGHTGRAY : DARKGRAY;
         DrawLine3D((Vector3){startX, 0, z}, (Vector3){endX, 0, z}, lineColor);
     }
+}
+
+void Renderer::DrawLine(const Vector3 &startPos, const Vector3 &endPos, Color color)
+{
+    ::DrawLine3D(startPos, endPos, color);
+}
+
+void Renderer::DrawPlane(const TransformComponent &transform, Color color)
+{
+    rlPushMatrix();
+
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
+
+    ::DrawPlane(transform.Position, {transform.Scale.x, transform.Scale.y}, color);
+
+    rlPopMatrix();
+}
+
+void Renderer::DrawPixel(const Vector2 &position, Color color)
+{
+    ::DrawPixelV(position, color);
+}
+
+void Renderer::DrawPoint(const Vector3 &position, Color color)
+{
+    ::DrawPoint3D(position, color);
+}
+
+void Renderer::DrawQuad(const TransformComponent &transform, Color color, float roundness,
+                        int segments)
+{
+    rlPushMatrix();
+
+    rlTranslatef(transform.Position.x, transform.Position.y, transform.Position.z);
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
+    rlScalef(transform.Scale.x, transform.Scale.y, 1.0f);
+
+    rlBegin(RL_QUADS);
+    rlColor4ub(color.r, color.g, color.b, color.a);
+    rlVertex3f(-0.5f, -0.5f, 0.0f);
+    rlVertex3f(0.5f, -0.5f, 0.0f);
+    rlVertex3f(0.5f, 0.5f, 0.0f);
+    rlVertex3f(-0.5f, 0.5f, 0.0f);
+    rlEnd();
+
+    rlBegin(RL_QUADS);
+    rlColor4ub(color.r, color.g, color.b, color.a);
+    rlVertex3f(-0.5f, -0.5f, 0.0f);
+    rlVertex3f(-0.5f, 0.5f, 0.0f);
+    rlVertex3f(0.5f, 0.5f, 0.0f);
+    rlVertex3f(0.5f, -0.5f, 0.0f);
+    rlEnd();
+
+    rlPopMatrix();
+}
+
+void Renderer::DrawQuadLines(const TransformComponent &transform, Color color,
+                             float roundness, int segments)
+{
+}
+
+void Renderer::DrawSphere(const TransformComponent &transform, Color color)
+{
+    rlPushMatrix();
+
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
+    rlScalef(transform.Scale.x, transform.Scale.y, transform.Scale.z);
+
+    ::DrawSphere(transform.Position, 1.0f, color);
+
+    rlPopMatrix();
+}
+
+void Renderer::DrawSphere(const TransformComponent &transform, int rings, int slices,
+                          Color color)
+{
+    rlPushMatrix();
+
+    rlRotatef(transform.Rotation.x, 1.0f, 0.0f, 0.0f);
+    rlRotatef(transform.Rotation.y, 0.0f, 1.0f, 0.0f);
+    rlRotatef(transform.Rotation.z, 0.0f, 0.0f, 1.0f);
+    rlScalef(transform.Scale.x, transform.Scale.y, transform.Scale.z);
+
+    ::DrawSphereEx(transform.Position, 1.0f, rings, slices, color);
+
+    rlPopMatrix();
+}
+
+void Renderer::DrawTriangle(const Vector3 &v1, const Vector3 &v2, const Vector3 &v3,
+                            Color color)
+{
+    ::DrawTriangle3D(v1, v2, v3, color);
+}
+
+void Renderer::DrawMesh(const TransformComponent &transform, const Mesh &mesh,
+                        const Material &material)
+{
+    ::DrawMesh(mesh, material, Matrix4::Transform(transform));
 }
 
 } // namespace Lumen
--- src/Lumen/Graphics/Renderer.hpp
+++ src/Lumen/Graphics/Renderer.hpp
@@ -2,13 +2,13 @@
 
 #include "Lumen/Core/Memory.hpp"
 #include "Lumen/Graphics/Color.hpp"
+#include "Lumen/Graphics/Material.hpp"
+#include "Lumen/Graphics/Mesh.hpp"
 #include "Lumen/Graphics/Shader.hpp"
 #include "Lumen/Math/Rectangle.hpp"
 #include "Lumen/Math/Vector2.hpp"
 #include "Lumen/Scene/Entity/Component/Core/TransformComponent.hpp"
 #include "Lumen/Scene/Entity/Component/Graphics/CameraComponent.hpp"
-
-#include <string>
 
 struct RenderTexture;
 
@@ -17,6 +17,9 @@ namespace Lumen
 
 class Renderer
 {
+public:
+    Renderer() = delete;
+
 public:
     static void BeginBlendMode();
     static void BeginCameraMode(const CameraComponent &camera);
@@ -39,13 +42,51 @@ public:
     static void SetRenderTexture(const ::RenderTexture texture);
 
     static void DrawFPS(const Vector2 position, int size, Color color);
-    static void DrawText();
 
-    static void DrawQuad(const TransformComponent &transform, Color color);
+    static void DrawCapsule(const TransformComponent &transform, int slices, int rings,
+                            Color color);
+    static void DrawCapsuleWire(const TransformComponent &transform, int slices,
+                                int rings, Color color);
+
+    static void DrawCircle(const TransformComponent &transform, Color color);
 
     static void DrawCube(const TransformComponent &transform, Color color);
+    static void DrawCubeWire(const TransformComponent &transform, Color color);
+
+    static void DrawCylinder(const TransformComponent &transform, int slices,
+                             Color color);
+    static void DrawCylinder(const TransformComponent &transform, float radiusTop,
+                             float radiusBottom, int slices, Color color);
+    static void DrawCylinderWire(const TransformComponent &transform, int slices,
+                                 Color color);
+    static void DrawCylinderWire(const TransformComponent &transform, float radiusTop,
+                                 float radiusBottom, int slices, Color color);
+
     static void DrawGrid(Vector3 cameraPosition, float minorSpacing, int majorDivisions,
                          int gridLines);
+
+    static void DrawLine(const Vector3 &startPos, const Vector3 &endPos, Color color);
+
+    static void DrawPlane(const TransformComponent &transform, Color color);
+
+    static void DrawPixel(const Vector2 &position, Color color);
+
+    static void DrawPoint(const Vector3 &position, Color color);
+
+    static void DrawQuad(const TransformComponent &transform, Color color,
+                         float roundness = 0.0f, int segments = 0.0f);
+    static void DrawQuadLines(const TransformComponent &transform, Color color,
+                              float roundness = 0.0f, int segments = 0.0f);
+
+    static void DrawSphere(const TransformComponent &transform, Color color);
+    static void DrawSphere(const TransformComponent &transform, int rings, int slices,
+                           Color color);
+
+    static void DrawTriangle(const Vector3 &v1, const Vector3 &v2, const Vector3 &v3,
+                             Color color);
+
+    static void DrawMesh(const TransformComponent &transform, const Mesh &mesh,
+                         const Material &material);
 };
 
 } // namespace Lumen
