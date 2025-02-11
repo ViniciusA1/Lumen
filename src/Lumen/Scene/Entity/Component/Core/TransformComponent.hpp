6fc4a2d1014237574d2201da0053e9244bc30e39
Vinicius Augusto <vinicius.borgue1@gmail.com>
2025-02-11 08:11:38 -0300

    refactor(Scene): Transform component don't have constructors now

--- src/Lumen/Scene/Entity/Component/Core/TransformComponent.hpp
+++ src/Lumen/Scene/Entity/Component/Core/TransformComponent.hpp
@@ -1,19 +1,15 @@
 #pragma once
 
 #include "Lumen/Math/Vector3.hpp"
-#include "Lumen/Scene/Entity/Component/ComponentBase.hpp"
 
 namespace Lumen
 {
 
-struct TransformComponent : public ComponentBase
+struct TransformComponent
 {
     Vector3 Position = {0.0f, 0.0f, 0.0f};
     Vector3 Rotation = {0.0f, 0.0f, 0.0f};
     Vector3 Scale = {1.0f, 1.0f, 1.0f};
-
-    TransformComponent() = default;
-    TransformComponent(const Vector3 &position) : Position(position) {}
 };
 
 } // namespace Lumen
