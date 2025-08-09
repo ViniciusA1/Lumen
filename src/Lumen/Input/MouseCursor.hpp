402e07371acbd6ce8dda606c5d5c27ed20187106
Vinicius Borgue <vinicius.borgue@visumdigital.com>
2025-08-09 18:40:51 -0300

    feat(Input): add MouseCursor enum

---
+++ src/Lumen/Input/MouseCursor.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace Lumen
+{
+
+enum class MouseCursor
+{
+    Default = 0,
+    Arrow = 1,
+    IBeam = 2,
+    Crosshair = 3,
+    PointingHand = 4,
+    ResizeEW = 5,
+    ResizeNS = 6,
+    ResizeNWSE = 7,
+    ResizeNESW = 8,
+    ResizeAll = 9,
+    NotAllowed = 10
+};
+
+} // namespace Lumen
