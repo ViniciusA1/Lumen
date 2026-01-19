6f06c57a3125e190afb391da76b4226ccef7ade2
Vinicius Augusto <vinicius.borgue1@gmail.com>
2026-01-18 21:50:28 -0300

    feat(Graphics): add new RenderTexture functions

--- src/Lumen/Graphics/RenderTexture.cpp
+++ src/Lumen/Graphics/RenderTexture.cpp
@@ -20,19 +20,54 @@ bool RenderTexture::IsValid() const
     return m_RenderTexture && ::IsRenderTextureValid(*m_RenderTexture);
 }
 
-Texture2D RenderTexture::GetColorTexture() const
-{
-    return &m_RenderTexture->texture;
-}
-
 Texture2D RenderTexture::GetDepthTexture() const
 {
     return &m_RenderTexture->depth;
 }
 
+int RenderTexture::GetHeight() const
+{
+    return m_RenderTexture->texture.height;
+}
+
 int RenderTexture::GetRendererID() const
 {
     return m_RenderTexture->id;
+}
+
+Vector2 RenderTexture::GetSize() const
+{
+    return {
+        static_cast<float>(m_RenderTexture->texture.width),
+        static_cast<float>(m_RenderTexture->texture.height),
+    };
+}
+
+Texture2D RenderTexture::GetTexture() const
+{
+    return &m_RenderTexture->texture;
+}
+
+int RenderTexture::GetWidth() const
+{
+    return m_RenderTexture->texture.width;
+}
+
+void RenderTexture::Resize(int width, int height)
+{
+    if (width <= 0 || height <= 0)
+        return;
+
+    if (GetWidth() != width || GetHeight() != height)
+    {
+        ::UnloadRenderTexture(*m_RenderTexture);
+        *m_RenderTexture = ::LoadRenderTexture(width, height);
+    }
+}
+
+void RenderTexture::Resize(const Vector2 &size)
+{
+    Resize(size.x, size.y);
 }
 
 RenderTexture::operator ::RenderTexture() const
--- src/Lumen/Graphics/RenderTexture.hpp
+++ src/Lumen/Graphics/RenderTexture.hpp
@@ -16,9 +16,15 @@ public:
 
     [[nodiscard]] bool IsValid() const;
 
-    [[nodiscard]] Texture2D GetColorTexture() const;
     [[nodiscard]] Texture2D GetDepthTexture() const;
+    [[nodiscard]] int GetHeight() const;
     [[nodiscard]] int GetRendererID() const;
+    [[nodiscard]] Vector2 GetSize() const;
+    [[nodiscard]] Texture2D GetTexture() const;
+    [[nodiscard]] int GetWidth() const;
+
+    void Resize(int width, int height);
+    void Resize(const Vector2 &size);
 
     operator ::RenderTexture() const;
     operator ::RenderTexture *() const;
