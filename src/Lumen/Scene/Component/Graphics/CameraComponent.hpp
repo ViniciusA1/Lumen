a1fa36ee6e9c95d18792a480a1cb5f0d2730c529
Vinicius Augusto <vinicius.borgue1@gmail.com>
2026-01-18 21:50:29 -0300

    feat(Scene): added fields to control near/far plane and orthographic

--- src/Lumen/Scene/Component/Graphics/CameraComponent.hpp
+++ src/Lumen/Scene/Component/Graphics/CameraComponent.hpp
@@ -13,18 +13,15 @@ enum class ProjectionType
 
 struct CameraComponent
 {
-    Vector3 Position = {10.0f, 10.0f, 10.0f};
-    Vector3 Target = {0.0f, 0.0f, 0.0f};
+    Vector3 Position = {0.0f, 0.0f, 0.0f};
+    Vector3 Target = {1.0f, 0.0f, 0.0f};
     Vector3 Up = {0.0f, 1.0f, 0.0f};
     float Fov = 60.0f;
+    float OrthographicSize = 5.0f;
+    float Near = 0.1f;
+    float Far = 1000.0f;
+    float AspectRatio = 16.0f / 9.0f;
     ProjectionType Projection = ProjectionType::Perspective;
-
-    CameraComponent() = default;
-    CameraComponent(const Vector3 &position, const Vector3 &target, const Vector3 &up,
-                    float fov, ProjectionType type)
-        : Position(position), Target(target), Up(up), Fov(fov), Projection(type)
-    {
-    }
 };
 
 } // namespace Lumen
